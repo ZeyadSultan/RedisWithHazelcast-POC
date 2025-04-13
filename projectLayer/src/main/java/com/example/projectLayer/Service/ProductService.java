a75c6059709dd95dde4b30c65bb600c72d790446
Zeyad Sultan <ziadkhaled2260.zk@gmail.com>
2025-04-13 20:43:11 +0200

    add cachingLayer as a dependency inside project layer

--- projectLayer/src/main/java/com/example/projectLayer/Service/ProductService.java
+++ projectLayer/src/main/java/com/example/projectLayer/Service/ProductService.java
@@ -2,15 +2,20 @@ package com.example.projectLayer.Service;
 
 import com.example.cachingLayer.mechanism.CachingMechanism;
 import com.example.projectLayer.model.Product;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class ProductService {
 
-
+    private final ApplicationContext context;
     private final CachingMechanism cachingMechanism;
 
-    public ProductService(CachingMechanism cachingMechanism) {
+    public ProductService(ApplicationContext applicationContext, CachingMechanism cachingMechanism) {
+        this.context = applicationContext;
         this.cachingMechanism = cachingMechanism;
     }
 
@@ -24,6 +29,16 @@ public class ProductService {
             return (Product) product;
         }
         return null;
+    }
+
+    //ensuring which cachingMechanism is instantiated and it is only one
+    @PostConstruct
+    public void listAllCachingBeans() {
+        String[] cachingBeans = context.getBeanNamesForType(CachingMechanism.class);
+        System.out.println("CachingMechanism beans registered:");
+        for(String beanName : cachingBeans) {
+            System.out.println(beanName);
+        }
     }
 
 }
